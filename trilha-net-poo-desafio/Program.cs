c6c7bf8bd23e8376f2bf3a0903d51335185085c8
DeividsonOmedio <114306758+DeividsonOmedio@users.noreply.github.com>
2022-12-14 17:17:36 -0300

    Add files via upload

---
+++ trilha-net-poo-desafio/Program.cs
@@ -0,0 +1,12 @@
+﻿using DesafioPOO.Models;
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "A22", imei: "987654321", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+Console.WriteLine("\n\nSmartphone Iphone");
+Smartphone iphone = new Iphone(numero: "123456", modelo: "14s", imei: "987654321", memoria: 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
+
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
